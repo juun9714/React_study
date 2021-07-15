74df40dad7c1ae808e1a1bb3187ba5ce00f05215
juun9714 <lovekwon200@gmail.com>
2021-07-15 18:08:21 +0900

    state

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import Subject from "./components/Subject"
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:'WEB', sub:"world wid web"},
+      content:{title:'HTML', desc:"HTML is HyperText Markup Language"}
+    }
+  }
+
   // render라는 메서드를 가지는 Component를 상속하는 App이라는 클래스
   render() {
     //Subject의 속성 -> property(title, sub)
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web !"></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
       </div>
     );
   }
