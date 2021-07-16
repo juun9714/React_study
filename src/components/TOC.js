16a5227d9b35ebbccb130e2fddf94c6c3522740a
juun9714 <lovekwon200@gmail.com>
2021-07-16 14:38:10 +0900

    change content with id

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,26 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class TOC extends Component{
-    render(){
-      var lists=[]
-      var data=this.props.data
-      var i=0
-      while(i<data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
-        i++
-      }
-      return(
-        <nav>
-          <ul>
-            {lists}
-              {/* <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li> */}
-          </ul>
-        </nav>
-      );
+class TOC extends Component {
+  render() {
+    var lists = []
+    var data = this.props.data
+    var i = 0
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              // debugger
+              // e.target.dataset.id
+              e.preventDefault()
+              this.props.onChangePage(e.target.dataset.id)
+            }.bind(this)}
+          >{data[i].title}</a>
+        </li>
+      )
+      i++
     }
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
   }
-  
+}
+
 export default TOC;
-// TOC.js 내의 구성요소 중 TOC를 외부에서 사용할 수 있게 하자 !
