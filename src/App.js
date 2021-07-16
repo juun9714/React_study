82bc601d20c7814e5f10ae335ba2158956548368
juun9714 <lovekwon200@gmail.com>
2021-07-16 12:35:14 +0900

    state change

--- src/App.js
+++ src/App.js
@@ -9,26 +9,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'read',
       subject: { title: 'WEB', sub: "world wid web" },
+      welcome:{title:"Welcome",desc:"Hello React !!!!"},
       contents:[
-        {id:1, title:"HTML",desc:"HTML is HyperText Markup Language"},
+        {id:1, title:"HTML",desc:"HTML is for information"},
         {id:2, title:"CSS",desc:"CSS is for design"},
         {id:3, title:"JavaScript",desc:"JavaScript is for interactive"}
       ],
-      content:{
-        title:"HTML", desc:"HTML is ooooo"
-      }
     }
   }
 
   // render라는 메서드를 가지는 Component를 상속하는 App이라는 클래스
   render() {
+    console.log("App render")
+    var _title, _desc=null;
+    if(this.state.mode==="welcome"){
+      _title=this.state.welcome.title
+      _desc=this.state.welcome.desc
+    }else if(this.state.mode==="read"){
+      _title=this.state.contents[0].title
+      _desc=this.state.contents[0].desc
+    }
     //Subject의 속성 -> property(title, sub)
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+                <h1><a href="/" onClick={function(e){
+                  console.log(e)
+                  e.preventDefault()
+                  this.state.mode="welcome"
+                  this.setState({
+                    mode:"welcome"
+                  })
+                }.bind(this)}>{this.state.subject.title}</a></h1>
+                {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -7,7 +7,7 @@ class Subject extends Component {
         return (
             // 컴포넌트는 반드시 하나의 최상위 태그로 시작해야 함
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
