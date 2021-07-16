525667956f88c4c1d625c24569b7bcbd613b38f2
juun9714 <lovekwon200@gmail.com>
2021-07-16 14:39:56 +0900

    change with bind parameter

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,12 +11,12 @@ class TOC extends Component {
           <a
             href={"/content/" + data[i].id}
             data-id={data[i].id}
-            onClick={function (e) {
+            onClick={function (id, e) {
               // debugger
               // e.target.dataset.id
               e.preventDefault()
-              this.props.onChangePage(e.target.dataset.id)
-            }.bind(this)}
+              this.props.onChangePage(id)
+            }.bind(this, data[i].id)}
           >{data[i].title}</a>
         </li>
       )
