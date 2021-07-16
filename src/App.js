9658b77d15b7b3ac838618262d69079c7cbd8494
juun9714 <lovekwon200@gmail.com>
2021-07-16 18:06:02 +0900

    making form tag

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: "world wid web" },
       welcome: { title: "Welcome", desc: "Hello React !!!!" },
--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -6,8 +6,24 @@ class CreateContent extends Component {
     return (
       <article>
         <h2>Create</h2>
-        <form>
-
+        <form action="/create_process" method="post"
+          onSubmit={function(e){
+            e.preventDefault()
+            alert("submit")
+            // 보통 form태그에서 submit 버튼이 눌리면, form의 action에서 지정한 페이지로 넘어가는게 default
+            // 하지만 우리는 페이지 전환없이 웹을 만드려고 하는 것이기 때문에 preventDefault해줌 
+            // action에서 지정한 페이지로 form에 입력한 내용 전송
+          }.bind(this)}
+        >
+          {/* onSubmit => html의 기능 */}
+          <p><input type="text" name="title" placeholder="title"></input></p>
+          <p>
+            <textarea name="desc" placeholder="description"></textarea>
+            {/* textarea <= 입력값이 여러 줄일 때 */}
+          </p>
+          <p>
+            <input type="submit" value="Submit"></input>
+          </p>
         </form>
       </article>
     );
